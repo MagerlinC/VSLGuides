6dcad21c5f7eacaa74dc99e1e320cc1a4008eb9b
Mikkel Agerlin Christensen <miach@ramboll.dk>
2018-04-05 15:55:13 +0200

    Begun work on animations & mobile gestures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'hammerjs';
 import { FormsModule} from '@angular/forms';
 import { HttpModule} from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
 import { RouterModule, Routes} from '@angular/router';
 import { GuidesComponent } from './guides/guides.component';
---
+++ src/app/keyframes.ts
@@ -0,0 +1,9 @@
+import {keyframes, style} from '@angular/animations';
+
+
+export const slideOutRight = [
+  style({transform: 'translate3d(0,0,0'}),
+  style({transform: 'translate3d(100%,0,0'}),
+]
+
+
